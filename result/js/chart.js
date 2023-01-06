381ec825c393b122b03a485f9f2bfc8438801768
artoshop <ajxsibu@gmail.com>
2023-01-05 21:44:41 -0800

    jhg

--- result/js/chart.js
+++ result/js/chart.js
@@ -35,11 +35,53 @@ var options1 = {
   }
 };
 
+var data2 = {
+  labels: ["Luminar", "Canva", "Sanpseed", "Picasa"],
+  
+  datasets: [{
+    label: "Total Point",
+    backgroundColor: ["#f20c14", "#0080ff", "#09eda0", "#f5b507"],
+    borderColor: "rgba(255,99,132,1)",
+    borderWidth: 1,
+    barThickness: 1,
+    data: [65, 56, 69, 98],
+    borderWidth: 1,
+    borderRadius: 3,
+
+  }]
+};
+
+var options2 = {
+  maintainAspectRatio: false,
+  responsive: false,
+  scales: {
+    yAxes: [{
+      stacked: true,
+      gridLines: {
+        display: true,
+        color: "rgba(255,99,132,0.2)"
+      }
+    }],
+    xAxes: [{
+      barThickness: 60,
+      maxBarThickness: 1000,
+      gridLines: {
+        display: false,
+      }
+    }]
+  }
+};
+
+
+
 Chart.Bar('chart-1', {
   options: options1,
   data: data1
 });
-
+Chart.Bar('chart-2', {
+  options: options2,
+  data: data2
+});
 
 setTimeout(
    function() { 
