b3fa26b9c463ade9907f26d27155262c477bdd31
artoshop <ajxsibu@gmail.com>
2023-01-07 07:38:39 -0800

    sdf

--- result/js/chart.js
+++ result/js/chart.js
@@ -7,7 +7,7 @@ var data1 = {
     borderColor: "rgba(255,99,132,1)",
     borderWidth: 1,
     barThickness: 1,
-    data: [180, 265, 218, 200],
+    data: [241, 318, 267, 326],
     borderWidth: 1,
     borderRadius: 3,
 
