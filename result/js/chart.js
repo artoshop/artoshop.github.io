397b4daccf84d180eed21f2478cedfd5a2264c07
artoshop <ajxsibu@gmail.com>
2023-01-11 08:28:54 +0530

    fgh

--- result/js/chart.js
+++ result/js/chart.js
@@ -7,7 +7,7 @@ var data1 = {
     borderColor: "rgba(255,99,132,1)",
     borderWidth: 1,
     barThickness: 1,
-    data: [519, 521, 563, 526],
+    data: [647, 631, 639, 640],
     borderWidth: 1,
     borderRadius: 3,
 
